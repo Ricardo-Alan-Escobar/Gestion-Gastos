837e51f1a323267c0c189a33c9471ac66e393acd
Ricardo Alan Escobar <ricardoalangomezescobar@gmail.com>
2025-07-21 12:28:25 -0600

    cambios en el w

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -17,7 +17,7 @@ export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <div className="flex h-auto flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className='w-full h-auto p-5 bg-white dark:bg-neutral-800 rounded-xl border-sidebar-border/70 dark:border-sidebar-border'>
                     <h1 className='text-3xl font-bold mb-2'>Gestión de pagos</h1>
                     <p>Administracion de pagos pendientes y programados.</p>
